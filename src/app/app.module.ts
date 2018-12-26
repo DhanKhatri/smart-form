c06686476d5ac386129c890dae2ed70bbe9aeb23
Dhan Khatri <db3khatri@gmail.com>
2018-12-25 21:52:29 -0600

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
